4edcdedee8d0b5236a9707878c336ca9043bb416
Mike England <micheallengland@gmail.com>
2024-11-05 10:07:51 -0500

    Add node_modules and build directory ignore
    Add two directories that I commonly need to ignore when searching files.
    NOTE: Later on, I'd like to find a way to easily toggle this on / off.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,6 +8,12 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       require("telescope").setup({
+        defaults = {
+          file_ignore_patterns={
+            "node_modules",
+            "build",
+          }
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
