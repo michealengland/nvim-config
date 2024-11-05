fedbef628e3ca479ae35ae346909235945f2b8a7
Mike England <micheallengland@gmail.com>
2024-11-05 08:29:57 -0500

    Update alpha config with latest example
    Originally, the Neovim font had some issues so I replaced it with text.
    Now, it' appears correctly.

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,17 +1,14 @@
 return {
   "goolord/alpha-nvim",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-
+  -- dependencies = { 'echasnovski/mini.icons' },
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    local alpha = require("alpha")
-    local dashboard = require("alpha.themes.startify")
-
-    dashboard.section.header.val = {
-      'ALPHA NVIM'
-    }
-
-    alpha.setup(dashboard.opts)
+    local startify = require("alpha.themes.startify")
+    -- available: devicons, mini, default is mini
+    -- if provider not loaded and enabled is true, it will try to use another provider
+    startify.file_icons.provider = "devicons"
+    require("alpha").setup(
+      startify.config
+    )
   end,
 }
