e3b5961372032f8756f56871104d4bc73911d82d
Mike England <micheallengland@gmail.com>
2024-11-24 10:12:03 -0500

    Cleanup telescope commands

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -21,14 +21,11 @@ return {
         },
       })
       local builtin = require("telescope.builtin")
-      -- vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      -- vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      -- vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
       require("telescope").load_extension("ui-select")
     end,
   },
