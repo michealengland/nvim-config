3f8a67d60fed6fd97ef7e5d40c41886d20b97305
Mike England <micheallengland@gmail.com>
2024-12-10 11:09:27 -0500

    Simplify telescope configuration
    - Reduce configuration down to barebones
    - Utilize navtive fzf for lightning fast search
    - Utilize original helper shortcuts

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,32 +1,14 @@
 return {
-  {
-    "nvim-telescope/telescope-ui-select.nvim",
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.8',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
-  {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.5",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      require("telescope").setup({
-        defaults = {
-          file_ignore_patterns={
-            "node_modules",
-            "build",
-          }
-        },
-        extensions = {
-          ["ui-select"] = {
-            require("telescope.themes").get_dropdown({}),
-          },
-        },
-      })
-      local builtin = require("telescope.builtin")
-      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
-      require("telescope").load_extension("ui-select")
-    end,
-  },
+  config = function()
+    vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files)
+    vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep)
+    vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers)
+    vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags)
+  end
 }
