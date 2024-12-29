3b344609a04da30164c39594394f2648a293daca
Mike England <micheallengland@gmail.com>
2024-12-29 12:11:16 -0500

    Replace catpuccin with rosepine theme

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,18 @@
+-- return {
+--   {
+--     "catppuccin/nvim",
+--     lazy = false,
+--     name = "catppuccin",
+--     priority = 1000,
+--     config = function()
+--       vim.cmd.colorscheme "catppuccin-mocha"
+--     end
+--   }
+-- }
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		vim.cmd("colorscheme rose-pine")
+	end
+}
