06af017fbb089e3ea36063523376d991445b1e54
Mike England <micheallengland@gmail.com>
2025-01-15 19:56:38 -0500

    Add missing fzf settings and play around with theme

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,6 +6,22 @@ return {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
   config = function()
+    require('telescope').setup {
+      defaults = {
+        file_ignore_patterns = { 'node_modules', '.git' },
+        layout_config = {
+          prompt_position = 'top',
+        },
+      },
+      pickers = {
+        find_files = {
+          theme = 'ivy',
+        },
+      },
+      extensions = {
+        fzf = {},
+      },
+    }
     vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files)
     vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep)
     vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers)
