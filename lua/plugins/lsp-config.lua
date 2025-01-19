09c10a2bb78c93edd72e4ebc3bf35388e2875e8e
Mike England <micheallengland@gmail.com>
2025-01-19 10:10:39 -0500

    Add experimental changes to lsp-config

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -12,31 +12,43 @@ return {
     opts = {
       auto_install = true,
     },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "astro",
+          "html",
+          "cssls",
+          "tailwindcss",
+          "lua_ls"
+        }
+      })
+    end
   },
   {
     "neovim/nvim-lspconfig",
-    lazy = false,
     config = function()
       local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
       local lspconfig = require("lspconfig")
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities
-      })
-      lspconfig.solargraph.setup({
-        capabilities = capabilities
-      })
+
       lspconfig.html.setup({
         capabilities = capabilities
       })
       lspconfig.lua_ls.setup({
         capabilities = capabilities
       })
+      lspconfig.ts_ls.setup({
+        capabilities = capabilities
+      })
+      lspconfig["astro"].setup({
+        capabilities = capabilities,
+        -- on_attach = on_attach,
+        filetypes = { "astro" }
+      })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
+    end
   },
 }
