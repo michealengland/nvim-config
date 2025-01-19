eecbd298ec8124374d7c014067d8e80776e97270
Mike England <micheallengland@gmail.com>
2025-01-19 09:22:01 -0500

    Cleanup dev theme plugin file

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,18 +1,7 @@
--- return {
---   {
---     "catppuccin/nvim",
---     lazy = false,
---     name = "catppuccin",
---     priority = 1000,
---     config = function()
---       vim.cmd.colorscheme "catppuccin-mocha"
---     end
---   }
--- }
 return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine-dawn")
-	end
+  "rose-pine/neovim",
+  name = "rose-pine",
+  config = function()
+    vim.cmd("colorscheme rose-pine-dawn")
+  end
 }
