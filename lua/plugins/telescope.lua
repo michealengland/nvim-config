983a9d68bc95d93ae34a3cece16881318b3efa74
Mike England <micheallengland@gmail.com>
2025-06-22 23:28:46 -0400

    Cleanup telescope plugin format

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,30 +1,31 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  tag = '0.1.8',
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
   dependencies = {
-    'nvim-lua/plenary.nvim',
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
   config = function()
-    require('telescope').setup {
+    require("telescope").setup({
       defaults = {
-        file_ignore_patterns = { 'node_modules', '.git', 'vendor' },
+        file_ignore_patterns = { "node_modules", ".git", "vendor" },
         layout_config = {
-          prompt_position = 'top',
+          prompt_position = "top",
         },
       },
       pickers = {
         find_files = {
-          theme = 'ivy',
+          theme = "ivy",
         },
       },
       extensions = {
         fzf = {},
       },
-    }
-    vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files)
-    vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep)
-    vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers)
-    vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags)
-  end
+    })
+    vim.keymap.set("n", "<leader>fb", require("telescope.builtin").buffers)
+    vim.keymap.set("n", "<leader>ff", require("telescope.builtin").find_files)
+    -- NOTE: live grep requires ripgrep binaries. Ex: `brew install ripgrep`
+    vim.keymap.set("n", "<leader>fg", require("telescope.builtin").live_grep)
+    vim.keymap.set("n", "<leader>fh", require("telescope.builtin").help_tags)
+  end,
 }
