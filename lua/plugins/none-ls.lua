5f35a44696a3621d87e1aa97bca838021b9762d5
Mike England <micheallengland@gmail.com>
2025-06-24 09:17:57 -0400

    Simplify comments and phpcbf config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -5,12 +5,12 @@ return {
     null_ls.setup({
       sources = {
         -- PHPCS diagnostics
+        -- Use `command = phpcs` for global OR `./vendor/bin/phpcs"` for project scoped.
+        -- Verify your PHPCS in the CL:
+        --   Global Scope:  phpcs --standard=WordPress-Core path/to/file.php
+        --   Project Scope: /vendor/bin/phpcs --standard=WordPress-Core path/to/file.php
+        -- NOTE: intelephense covers additional errors that PHPCS does not catch.
         null_ls.builtins.diagnostics.phpcs.with({
-          -- Use command = phpcs for global or command = "./vendor/bin/phpcs" for project scoped.
-          -- Verify your PHPCS works:
-          --   Global Scope:  phpcs --standard=WordPress-Core path/to/file.php
-          --   Project Scope: /vendor/bin/phpcs --standard=WordPress-Core path/to/file.php
-          --   NOTE: intelephense covers additional errors that PHPCS does not catch.
           command = "./vendor/bin/phpcs",
           filetypes = { "php" },
         }),
@@ -18,13 +18,7 @@ return {
         -- PHPCBF PHP Code Beautifier (Formatter)
         null_ls.builtins.formatting.phpcbf.with({
           command = "./vendor/bin/phpcbf",
-          args = {
-            -- Not specifiying a standard defaults phpcs.xml.
-            -- "--standard=WordPress-Extra",
-            "-",
-          },
-          to_stdin = true,
-          extra_args = {}, -- <- Silences most warnings
+          filetypes = { "php" },
         }),
 
         -- Stylua (Formatter)
