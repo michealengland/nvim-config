c1fbac03bd0dcf2c3e9bb46f33de41f99de995fd
Mike England <micheallengland@gmail.com>
2025-06-24 00:54:27 -0400

    Cleanup init.lua formatting

--- init.lua
+++ init.lua
@@ -1,13 +1,13 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -17,6 +17,6 @@ require("lazy").setup("plugins")
 -- Neovim Diagnostic Options
 -- https://neovim.io/doc/user/diagnostic.html#vim.diagnostic.Opts
 vim.diagnostic.config({
-  update_in_insert = true,  -- Update diagnostics in Insert mode
-  virtual_text = true,      -- Render inline error text
+  update_in_insert = true, -- Update diagnostics in Insert mode
+  virtual_text = true,    -- Render inline error text
 })
